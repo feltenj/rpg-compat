85d0bf40df9d708a5d7dc2e598770119c5e4dd45
janinefelten <janine.felten92@gmail.com>
2024-06-12 11:57:35 +0200

    character can heal

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,9 +1,18 @@
-class Character(var health: Int) {
-    fun damage(damage: Int) {
+class Character(var health: Int = 1000, var level: Int = 1) {
+    fun damage(character: Character, damage: Int) {
+        if (character === this) {
+            return
+        }
         health -= damage
         if (health < 0) {
             health = 0
         }
+    }
+
+    fun heal() {
+        if (!isAlive) return
+        health = if (level > 5) 1500 else 1000
+
     }
 
     val isAlive: Boolean
--- src/test/kotlin/CharacterTest.kt
+++ src/test/kotlin/CharacterTest.kt
@@ -4,59 +4,82 @@ import org.junit.jupiter.api.Test
 class CharacterTest {
 
     @Test
-    fun testCharacterCreation() {
-        val character = Character(100)
-        assertNotNull(character, "Character should not be null")
-    }
-
-    @Test
-    fun `character should be created with default health 100`() {
-        val character = Character(health = 100)
-        assertEquals(100, character.health)
+    fun `character should be created with default health 1000`() {
+        val character = Character()
+        assertEquals(1000, character.health)
     }
 
     @Test
     fun `character should be alive when initialized`() {
-        val character = Character(health = 100)
+        val character = Character()
         assertTrue(character.isAlive)
     }
 
     @Test
-    fun `damage() should substract health of character `() {
-        val character = Character(health = 100)
-        character.damage(10)
-        assertEquals(90, character.health)
-    }
-
-    @Test
     fun `isAlive should return false when health is 0`() {
-        val character = Character(health = 0)
+        val character = Character(health = 0, level = 1)
         assertFalse(character.isAlive)
     }
 
     @Test
     fun `isAlive should return false when health is negative`() {
-        val character = Character(health = -1)
+        val character = Character(health = -1, level = 1)
         assertFalse(character.isAlive)
     }
 
     @Test
     fun `isAlive should return true when health is positive`() {
-        val character = Character(health = 1)
+        val character = Character(health = 1, level = 1)
         assertTrue(character.isAlive)
     }
 
     @Test
-    fun `damage() should set health not smaller than 0`() {
-        val character = Character(health = 10)
-        character.damage(20)
+    fun `damage() should subtract health of character2`() {
+        val character = Character()
+        val character2 = Character()
+        character2.damage(character, 100)
+        assertEquals(900, character2.health)
+    }
+
+    @Test
+    fun `damage() should set character2 health not smaller than 0`() {
+        val character = Character()
+        val character2 = Character()
+        character2.damage(character, 1200)
+        assertEquals(0, character2.health)
+    }
+
+    @Test
+    fun `damage() cannot damage character2 itself`() {
+        val character2 = Character()
+        character2.damage(character2, 100)
+        assertEquals(1000, character2.health)
+    }
+
+    @Test
+    fun `heal() sets health to initial value`() {
+        val character = Character(health = 10, level = 1)
+        character.heal()
+        assertEquals(1000, character.health)
+    }
+
+    @Test
+    fun `heal() should not heal when dead`() {
+        val character = Character(health = 0, level = 1)
+        character.heal()
         assertEquals(0, character.health)
     }
 
-//    @Test
-//    fun `damage() cannot damage character itself`() {
-//        val character = Character(health = 100)
-//        character.damage(character, 10)
-//        assertEquals(100, character.health)
-//    }
+    @Test
+    fun `character should be created with level 1`() {
+        val character = Character()
+        assertEquals(1, character.level)
+    }
+
+    @Test
+    fun `heal() sets health to 1500 when greater than level 5`() {
+        val character = Character(health = 10, level = 6)
+        character.heal()
+        assertEquals(1500, character.health)
+    }
 }
